2a85bfbf952c77c580fe02767fd5393a21a93969
cherry7 <cherry.chen@55haitao.com>
2022-06-09 21:59:50 +0800

    feat:  Notes

--- 排序/冒泡排序.js
+++ 排序/冒泡排序.js
@@ -7,6 +7,9 @@ function bubbleSort(arr){
   let len = arr.length
   for (let i = 0; i < len; i++) {
     let flag = false
+    //从左到右冒泡（小到大排序）
+    //处理过的数，就不用再处理所以j每一次都递减1
+    //即 len-i-1
     for (let j = 0; j < len-1-i; j++) {
       if (arr[j] > arr[j+1]) {
           swap(arr,j,j+1)
