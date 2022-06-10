5f8e5488bf38c2d2760b8e0352688febbbe98b97
CCherry07 <2405693142@qq.com>
2022-06-10 22:27:34 +0800

    feat:  update testCase

--- 1. 两数之和.js
+++ 1. 两数之和.js
@@ -4,11 +4,17 @@
  * @return {number[]}
  */
  const twoSum = function(nums, target) {
-      let numMap = new Map()
+      let numMap = new Map() 
+    // 将，能和nums[i]相加等于target的值为key，以及自身的索引为value
     for (let i = 0; i < nums.length; i++) {
+      // 获取 能和nums[i]相加等于target的值
         let find = target-nums[i]
+        // 在map里寻找，是否有以nums[i]为key的
+        // 如果有就输出[numMap.get[nums[i]],i]
+        // 此时 numMap.get[nums[i]] 是nums[i]的正确索引 , i为find的正确索引
+        // 如果没有保存 find , 以及nums[i] 的索引
             if (numMap.has(nums[i])) {
-              return [i,numMap.get(nums[i])]
+              return [numMap.get(nums[i]),i]
             }else{
               numMap.set(find,i)
             }
