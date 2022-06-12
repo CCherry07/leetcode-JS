6a231af6d8a605e5c74d2105b5c8541106b71ca7
CCherry07 <2405693142@qq.com>
2022-06-12 22:40:59 +0800

    feat:  threeSum

---
+++ 15. 三数之和.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = function(nums) {
+  if(nums == null || nums.length < 3) return [];
+  let resArr=[]
+  nums.sort((a,b)=>a-b)
+  for(let i=0;i<nums.length-2;i++){
+    if (i===0 || nums[i] !==nums[i-1]) {
+    let left = i +1 , right = nums.length-1
+    while(left < right){
+      const sum = nums[left] + nums[right] + nums[i]
+      if (sum === 0 ) {
+        resArr.push([nums[i],nums[left],nums[right]])
+        left++
+        right--
+        while (left < right && nums[left] === nums[left - 1]) left++
+        while (left < right && nums[right] === nums[right + 1]) right--
+      }else if( sum > 0 ) {
+        right--
+      }else{
+        left++
+      }
+    }
+  }
+}
+  return resArr
+};
+
+let nums = [-1,0,1,2,-1,-4]
+console.log(threeSum(nums));
